92352fdec920e1bf3046c55aad894e4be28458df
jicksaw <jicksaw@pm.me>
2021-09-16 05:08:13 +0300

    Fix indentation

--- Ticker.lua
+++ Ticker.lua
@@ -1,93 +1,93 @@
 function CEPGP_PeriodicEP:TickerStart()
-  if self.tickerState ~= "stopped" then
-    return
-  end
+    if self.tickerState ~= "stopped" then
+	return
+    end
 
-  self.tickerState = "running"
-  start_periodic_ep_button:Hide()
-  pause_periodic_ep_button:Show()
+    self.tickerState = "running"
+    start_periodic_ep_button:Hide()
+    pause_periodic_ep_button:Show()
 
-  self:CancelTimer(self.tickerTimer)
-  self.tickerTimer = self:ScheduleRepeatingTimer(self.TickerTimerCallback, self.db.profile.period * 60, self)
+    self:CancelTimer(self.tickerTimer)
+    self.tickerTimer = self:ScheduleRepeatingTimer(self.TickerTimerCallback, self.db.profile.period * 60, self)
 
-  local message =
-    "Periodic EP started: " .. self.db.profile.amount .. " EP every " .. self.db.profile.period .. " minutes"
-  CEPGP_sendChatMessage(message, CEPGP.Channel)
+    local message =
+	"Periodic EP started: " .. self.db.profile.amount .. " EP every " .. self.db.profile.period .. " minutes"
+    CEPGP_sendChatMessage(message, CEPGP.Channel)
 end
 
 function CEPGP_PeriodicEP:TickerPause()
-  if self.tickerState ~= "running" then
-    return
-  end
+    if self.tickerState ~= "running" then
+	return
+    end
 
-  self.tickerState = "paused"
-  pause_periodic_ep_button:Disable()
+    self.tickerState = "paused"
+    pause_periodic_ep_button:Disable()
 
-  self.ticker_timer_remaining = self:TimeLeft(self.tickerTimer)
-  self.ticker_timer_leftover = self.db.profile.period * 60 - self.ticker_timer_remaining
-  self:CancelTimer(self.tickerTimer)
-  periodic_ep_ticker_paused_dialog:Show()
+    self.ticker_timer_remaining = self:TimeLeft(self.tickerTimer)
+    self.ticker_timer_leftover = self.db.profile.period * 60 - self.ticker_timer_remaining
+    self:CancelTimer(self.tickerTimer)
+    periodic_ep_ticker_paused_dialog:Show()
 
-  local message = "Periodic EP paused"
-  CEPGP_sendChatMessage(message, CEPGP.Channel)
+    local message = "Periodic EP paused"
+    CEPGP_sendChatMessage(message, CEPGP.Channel)
 end
 
 function CEPGP_PeriodicEP:TickerResume()
-  if self.tickerState ~= "paused" then
-    return
-  end
+    if self.tickerState ~= "paused" then
+	return
+    end
 
-  self.tickerState = "running"
-  pause_periodic_ep_button:Enable()
+    self.tickerState = "running"
+    pause_periodic_ep_button:Enable()
 
-  self.tickerTimer = self:ScheduleTimer(self.TickerTimerResumeCallback, self.ticker_timer_remaining, self)
-  periodic_ep_ticker_paused_dialog:Hide()
+    self.tickerTimer = self:ScheduleTimer(self.TickerTimerResumeCallback, self.ticker_timer_remaining, self)
+    periodic_ep_ticker_paused_dialog:Hide()
 
-  local message = "Periodic EP resumed"
-  CEPGP_sendChatMessage(message, CEPGP.Channel)
+    local message = "Periodic EP resumed"
+    CEPGP_sendChatMessage(message, CEPGP.Channel)
 end
 
 function CEPGP_PeriodicEP:TickerStop()
-  if self.tickerState ~= "paused" then
-    return
-  end
+    if self.tickerState ~= "paused" then
+	return
+    end
 
-  self.tickerState = "stopped"
-  pause_periodic_ep_button:Hide()
-  pause_periodic_ep_button:Enable()
-  start_periodic_ep_button:Show()
+    self.tickerState = "stopped"
+    pause_periodic_ep_button:Hide()
+    pause_periodic_ep_button:Enable()
+    start_periodic_ep_button:Show()
 
-  periodic_ep_ticker_paused_dialog:Hide()
-  periodic_ep_ticker_stopped_dialog:Show()
+    periodic_ep_ticker_paused_dialog:Hide()
+    periodic_ep_ticker_stopped_dialog:Show()
 end
 
 function CEPGP_PeriodicEP:TickerAwardLeftover(minutes)
-  start_periodic_ep_button:Enable()
-  periodic_ep_ticker_stopped_dialog:Hide()
+    start_periodic_ep_button:Enable()
+    periodic_ep_ticker_stopped_dialog:Hide()
 
-  if minutes <= 0 then
-    CEPGP_sendChatMessage("Periodic EP stopped", CEPGP.Channel)
-    return
-  end
+    if minutes <= 0 then
+	CEPGP_sendChatMessage("Periodic EP stopped", CEPGP.Channel)
+	return
+    end
 
-  local EP = floor(minutes * (self.db.profile.amount / self.db.profile.period) + 0.5)
-  CEPGP_AddRaidEP(EP, "Periodic EP stopped")
-  if STANDBYEP and tonumber(STANDBYPERCENT) > 0 then
-    CEPGP_addStandbyEP(EP * (tonumber(STANDBYPERCENT) / 100), nil, "Periodic EP")
-  end
+    local EP = floor(minutes * (self.db.profile.amount / self.db.profile.period) + 0.5)
+    CEPGP_AddRaidEP(EP, "Periodic EP stopped")
+    if STANDBYEP and tonumber(STANDBYPERCENT) > 0 then
+	CEPGP_addStandbyEP(EP * (tonumber(STANDBYPERCENT) / 100), nil, "Periodic EP")
+    end
 end
 
 function CEPGP_PeriodicEP:TickerTimerCallback()
-  CEPGP_AddRaidEP(self.db.profile.amount, "Periodic EP")
-  if STANDBYEP and tonumber(STANDBYPERCENT) > 0 then
-    CEPGP_addStandbyEP(self.db.profile.amount * (tonumber(STANDBYPERCENT) / 100), nil, "Periodic EP")
-  end
+    CEPGP_AddRaidEP(self.db.profile.amount, "Periodic EP")
+    if STANDBYEP and tonumber(STANDBYPERCENT) > 0 then
+	CEPGP_addStandbyEP(self.db.profile.amount * (tonumber(STANDBYPERCENT) / 100), nil, "Periodic EP")
+    end
 end
 
 function CEPGP_PeriodicEP:TickerTimerResumeCallback()
-  CEPGP_AddRaidEP(self.db.profile.amount, "Periodic EP")
-  if STANDBYEP and tonumber(STANDBYPERCENT) > 0 then
-    CEPGP_addStandbyEP(self.db.profile.amount * (tonumber(STANDBYPERCENT) / 100), nil, "Periodic EP")
-  end
-  self.tickerTimer = self:ScheduleRepeatingTimer(self.TickerTimerCallback, self.db.profile.period * 60, self)
+    CEPGP_AddRaidEP(self.db.profile.amount, "Periodic EP")
+    if STANDBYEP and tonumber(STANDBYPERCENT) > 0 then
+	CEPGP_addStandbyEP(self.db.profile.amount * (tonumber(STANDBYPERCENT) / 100), nil, "Periodic EP")
+    end
+    self.tickerTimer = self:ScheduleRepeatingTimer(self.TickerTimerCallback, self.db.profile.period * 60, self)
 end
--- Warning.lua
+++ Warning.lua
@@ -1,27 +1,27 @@
 local L = LibStub("AceLocale-3.0"):GetLocale("CEPGP_PeriodicEP", true)
 
 function CEPGP_PeriodicEP:RegisterWarnings()
-  StaticPopupDialogs["PERIODIC_EP_NOT_STARTED"] = {
-    text = L["Start periodic EP?"],
-    button1 = "Yes",
-    button2 = "No",
-    OnAccept = function()
-      CEPGP_PeriodicEP:TickerStart()
-    end,
-    timeout = 0,
-    whileDead = true,
-    hideOnEscape = true,
-    preferredIndex = 3,
-  }
+    StaticPopupDialogs["PERIODIC_EP_NOT_STARTED"] = {
+	text = L["Start periodic EP?"],
+	button1 = "Yes",
+	button2 = "No",
+	OnAccept = function()
+	    CEPGP_PeriodicEP:TickerStart()
+	end,
+	timeout = 0,
+	whileDead = true,
+	hideOnEscape = true,
+	preferredIndex = 3,
+    }
 
-  self:RegisterEvent("PLAYER_ENTERING_WORLD", "EnteredInstance")
-  -- Call it once in case we're already in an instance
-  self:EnteredInstance()
+    self:RegisterEvent("PLAYER_ENTERING_WORLD", "EnteredInstance")
+    -- Call it once in case we're already in an instance
+    self:EnteredInstance()
 end
 
 function CEPGP_PeriodicEP:EnteredInstance()
-  local _, type, _, _, _, _, _, instanceID = GetInstanceInfo()
-  if self.db.profile.warnIfNotRunning[instanceID] and self.tickerState ~= "running" then
-    StaticPopup_Show("PERIODIC_EP_NOT_STARTED")
-  end
+    local _, type, _, _, _, _, _, instanceID = GetInstanceInfo()
+    if self.db.profile.warnIfNotRunning[instanceID] and self.tickerState ~= "running" then
+	StaticPopup_Show("PERIODIC_EP_NOT_STARTED")
+    end
 end
